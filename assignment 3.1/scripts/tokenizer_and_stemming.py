d7206a3c8bd02526dee4337d3da1a1b77b17b35b
yongwei12 <yongwei996@gmail.com>
2020-10-01 13:57:23 +0800

    updated tokenizing code

--- assignment 3.1/scripts/tokenizer_and_stemming.py
+++ assignment 3.1/scripts/tokenizer_and_stemming.py
@@ -3,28 +3,37 @@ import os
 
 # function to convert the tokenizer output style
 def convert(lst):   
-    return '|'.join(lst) 
+    return '\n'.join(lst) 
 
 # declare the directory
 directory = "./data/sigbovik/"
-output = "./output/sigbovik/Tokenizer"
+outputTokenizer = "./output/sigbovik/Tokenizer"
+outputStemming= "./output/sigbovik/Stemming"
 
 # function to read files in directory and tokenize it
-def tokenizeFile (input, output=0):
+def tokenizeFile (input, output=0,combine=False ):
     for filename in os.listdir(directory):
         if filename.endswith(".txt"):
             f = open(os.path.join(directory, filename), "r",  encoding="utf8") 
-            Lines = f.readlines()
-            file_out =open(os.path.join(output, filename), "w", encoding="utf8")
-            for line in Lines:
-                file_out.write(convert(word_tokenize(line))+"\n")
+            text = f.read()
+            if combine == False:
+                file_out =open(os.path.join(output, filename), "w", encoding="utf8")
+                tokenize_list =list(dict.fromkeys(word_tokenize(text)))
+                file_out.write(convert(tokenize_list)+"\n")
+            else:
+                file_out =open(os.path.join(output, "combined_files.txt"), "a+", encoding="utf8")
+                tokenize_list =list(dict.fromkeys(word_tokenize(text)))
+                file_out.write(convert(tokenize_list)+"\n") 
             continue
         else:
             continue
 
+#Function to stem the files 
+
+
 
 #calling the tokenizer function
-tokenizeFile(directory,output )
+tokenizeFile(directory,outputTokenizer,True )
 
 
 
@@ -33,6 +42,7 @@ tokenizeFile(directory,output )
 # my_file = "./scripts/demo.txt"
 # f = open(my_file, "r",  encoding="utf8")
 # Lines = f.readlines()
+# print(f.read())
 # file_out =open(os.path.join(output, "demo.txt"), "w", encoding="utf8")
 # for line in Lines:
 #     file_out.write(convert(word_tokenize(line))+"\n")
