334c1f16ee4c893671660d3306da78c52a41989b
yongwei12 <yongwei996@gmail.com>
2020-10-01 12:54:24 +0800

    working tokenizer

--- assignment 3.1/scripts/sigbovik_tokenizer.py
+++ assignment 3.1/scripts/sigbovik_tokenizer.py
@@ -4,25 +4,40 @@ import os
 
 # example_text = "Hi here, I am a boy.  Mr. Smith says you are a loser \n asdsasdsasda"
 
+def convert(lst):   
+    return '|'.join(lst) 
 
 # iterating through the whole dataset
 directory = "./data/sigbovik/"
-def readFiles (input, output):
-    for filename in os.listdir(directory):
-        if filename.endswith(".txt"): 
-            # do tokenisation here
+output = "./output/sigbovik"
 
+# function to read files in directory and tokenize it
+def tokenizeFile (input, output=0):
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            f = open(os.path.join(directory, filename), "r",  encoding="utf8") 
+            Lines = f.readlines()
+            file_out =open(os.path.join(output, filename), "w", encoding="utf8")
+            for line in Lines:
+                file_out.write(convert(word_tokenize(line))+"\n")
             continue
         else:
             continue
 
 
+
+tokenizeFile(directory,output )
+
+
+
+# try out code
 # print(os.listdir())
-# my_file = "./data/sigbovik/2017_batch_norm_my_ass.txt"
+# my_file = "./scripts/demo.txt"
 # f = open(my_file, "r",  encoding="utf8")
 # Lines = f.readlines()
+# file_out =open(os.path.join(output, "demo.txt"), "w", encoding="utf8")
 # for line in Lines:
-#     print(line)
+#     file_out.write(convert(word_tokenize(line))+"\n")
 
 
 # for i in word_tokenize(example_text):
